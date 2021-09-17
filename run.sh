6e6987895aaa13bd6ace7453f8c386a18951aa7e
mengjian0502 <jmeng15@asu.edu>
2021-09-17 04:19:09 -0400

    modified

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-PYTHON="/home/jmeng15/anaconda3/envs/ssd_quant/bin/python"
+PYTHON="/home/li/.conda/envs/neurosim_test/bin/python"
 ############ directory to save result #############
 
 if [ ! -d "$DIRECTORY" ]; then
@@ -6,26 +6,18 @@ if [ ! -d "$DIRECTORY" ]; then
     mkdir ./dataset
 fi
 export CUDA_VISIBLE_DEVICES=1
-num_runs=6
 model=resnet50
 
 dataset=wikiart
-data_path='/home/jmeng15/imagenet_to_sketch/wikiart'
+data_path='/home/li/data/imagenet_to_sketch/wikiart'
 # data_path='/opt/imagenet/imagenet_compressed/'
 
-epochs=30
 batch_size=1
-optimizer=SGD
 
 # quantization scheme
 channel_wise=0
 wbit=4
 abit=4
-
-wd=1e-4
-lr=5e-3
-
-pretrained_model="./save/resnet50/resnet50_lr0.005_wd0.0001_channelwise0/model_best.pth.tar"
 
 save_path="./save/${model}_CL/${dataset}/${model}_lr${lr}_wd${wd}_channelwise${channel_wise}/"
 log_file="${model}_lr${lr}_wd${wd}_wbit${wbit}_abit${abit}_run${run_id}.log"
@@ -39,5 +31,6 @@ $PYTHON -W ignore main.py --dataset ${dataset} \
     --ngpu 1 \
     --wbit ${wbit} \
     --abit ${abit} \
+    --fine_tune
 
     
